7ec34740370fd58b62a24d188e022d766f19265b
puliwang <puliwang@exiao.tech>
2022-07-28 10:39:23 +0800

    1

--- projects/form-designer/drop-container/directives/qflow-optional-components.directive.ts
+++ projects/form-designer/drop-container/directives/qflow-optional-components.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 import SortableJs from 'sortablejs';
 
 @Directive({
@@ -6,12 +6,17 @@ import SortableJs from 'sortablejs';
 })
 export class QflowOptionalComponentsDirective implements OnInit {
 
+  @Input('qflowOptionalComponents')
+  list?: any[];
+  @Input()
+  dragItemClass!: string;
   constructor(
     private el: ElementRef
   ) { }
 
   ngOnInit(): void {
     console.log('el:', this.el.nativeElement);
+    console.log('list:', this.list);
     SortableJs.create(this.el.nativeElement, {
       group: {
         name: 'form-designer',
@@ -19,9 +24,39 @@ export class QflowOptionalComponentsDirective implements OnInit {
         put: false
       },
       sort: false,
+      setData: (/** DataTransfer */dataTransfer, /** HTMLElement*/dragEl: HTMLElement) => {
+        console.log('set data dragEl:', dragEl);
+        // console.log('set data dataTransfer:', dataTransfer);
+        // // dataTransfer.setData('Text', dragEl.textContent); // `dataTransfer` object of HTML5 DragEvent
+        // dataTransfer.setData('Text', '1235453'); // `dataTransfer` object of HTML5 DragEvent
+        console.log('set data dragEl:', dragEl.parentNode);
+        const arr = dragEl.parentNode?.querySelectorAll(`.${this.dragItemClass}`)
+        // console.log('this:',this.dragItemClass);
+        console.log('arr:', arr);
+      },
       onStart: function (/**Event*/evt) {
         // evt.oldIndex;  // element index within parent
-        console.log('stat:', evt);
+        // console.log('stat:', evt);
+      },
+      onEnd(e: any) {
+        var itemEl = e.item;  // dragged HTMLElement
+        // e.to;    // target list
+        // e.from;  // previous list
+        // e.oldIndex;  // element's old index within old parent
+        // e.newIndex;  // element's new index within new parent
+        // e.oldDraggableIndex; // element's old index within old parent, only counting draggable elements
+        // e.newDraggableIndex; // element's new index within new parent, only counting draggable elements
+        // e.clone // the clone element
+        // e.pullMode;  // when item is in another sortable: `"clone"` if cloning, `true` if moving
+        console.log('end:', (e.originalEvent as DragEvent).dataTransfer?.getData('Text'));
+        console.log('itemEl:', itemEl);
+        let condition = true // Your condition here
+        if (condition) {
+          const items = e.from.querySelectorAll(':scope > div') // You can change this if needed (example: draggable: '.my-item' change to :scope > .my-item)
+          e.from.insertBefore(e.item, items[e.oldIndex + (e.oldIndex > e.newIndex)])
+          return false
+        }
+        return true;
       },
       onMove: function (/**Event*/evt, /**Event*/originalEvent) {
         // // Example: https://jsbin.com/nawahef/edit?js,output
@@ -36,12 +71,12 @@ export class QflowOptionalComponentsDirective implements OnInit {
         // // return 1; — insert after target
         // // return true; — keep default insertion point based on the direction
         // // return void; — keep default insertion point based on the direction
-        console.log('move:', evt);
+        // console.log('move:', evt);
       },
       onChange: function (/**Event*/evt) {
         // evt.newIndex // most likely why this event is used is to get the dragging element's current index
         // same properties as onEnd
-        console.log('change:', evt);
+        // console.log('change:', evt);
       }
     });
   }
